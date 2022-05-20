4ad1731969eef42b60e564a0ab7f0a544209cefd
João Vitor <joaovitordesousapereira@gmail.com>
2022-05-20 15:38:03 -0300

    commitado junto com o sbpay

---
+++ sbpay/src/main/java/br/com/jvs/bluetasks/Sbpay1Application.java
@@ -0,0 +1,13 @@
+package br.com.jvs.bluetasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbpay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbpay1Application.class, args);
+	}
+
+}
